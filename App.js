d37edd7d0ebd2ffc54e84696de5279582c129fab
kco <kco796@naver.com>
2022-07-11 01:37:40 +0900

    BottomTab 생성

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, Text} from 'react-native';
 import Webtoon from './components/Webtoon';
+import Setting from './components/Setting';
 
-const App = () => {
+const Tab = createMaterialBottomTabNavigator();
+const WebtoonScreen = () => {
   return <Webtoon></Webtoon>;
+};
+const SettingScreen = () => {
+  return <Setting></Setting>;
+};
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="웹툰" component={WebtoonScreen}></Tab.Screen>
+        <Tab.Screen name="SettingScreen" component={SettingScreen}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
